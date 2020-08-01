79a56ecfdde4d7947b12fcbf83f570090ccad3c6
Nate <nrstone@uab.edu>
2020-08-01 10:04:03 -0500

    Adjust packges to new babel methods

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -17,7 +17,7 @@ const config = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query:{
-                presets: ['react','es2015']
+                presets: ['@babel/preset-env', '@babel/preset-react']
             }
         }]
     }
